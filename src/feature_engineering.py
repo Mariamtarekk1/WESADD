29efd57d42884b302d328e1305553b1fdf51f8fe
Mariamtarekk1 <mariamt6876@gmail.com>
2026-04-28 16:38:52 +0300

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,1 +1,11 @@
+#(Feature Engineering)
+#Windowing + Aggregation
+#The raw physiological signals were segmented into fixed-size windows (50 samples per window).Each window was then summarized using the mean value to reduce dimensionality and create meaningful features.
+# 0, 6, 7 not important
+#using 1 = baseline , 2 = stress , 3 = amusement , 4 = meditation
+#كل label فيها كام sample
+unique, counts = np.unique(labels, return_counts=True)
 
+for u, c in zip(unique, counts):
+    print(f"Label {u}: {c}")
+  
