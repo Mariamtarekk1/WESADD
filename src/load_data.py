456a71db30d275981ef623ca4b113b05a08a91cf
Mariamtarekk1 <mariamt6876@gmail.com>
2026-04-28 16:30:45 +0300

    Update load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -1,1 +1,19 @@
+mport pandas as pd
+import os
+import pickle
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+### Loading Dataset
 
+#The WESAD dataset consists of multiple subjects, each containing physiological signals and labels.
+### Loading Data
+from google.colab import drive
+drive.mount('/content/drive')
+import zipfile
+import os
+
+zip_path = '/content/drive/MyDrive/data WESAD.zip'
+extract_path = '/content/WESAD_data'
+
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall(extract_path)
