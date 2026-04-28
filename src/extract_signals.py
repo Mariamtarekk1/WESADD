b47c47d19569922132212862263ed972690d8284
Mariamtarekk1 <mariamt6876@gmail.com>
2026-04-28 17:34:21 +0300

    Update extract_signals.py

--- src/extract_signals.py
+++ src/extract_signals.py
@@ -24,14 +24,35 @@ subject = data['subject']
 print("Subject:", subject)
 print("Labels shape:", labels.shape)
 
+# استخراج التسميات الصالحة فقط
+valid_labels = [1, 2, 3, 4]
+mask = np.isin(labels, valid_labels)
+
+# تطبيق الاستخراج على إشارات الصدر
+filtered_chest = {}
+for key in chest.keys():
+    filtered_chest[key] = chest[key][mask]
 print("\nChest signals shapes:")
+
 for key in chest.keys():
     print(key, chest[key].shape)
 
 print("\nWrist signals shapes:")
 for key in wrist.keys():
     print(key, wrist[key].shape)
-  import numpy as np
+import pandas as pd
+
+df = pd.DataFrame({
+    'ACC_x': filtered_chest['ACC'][:, 0],
+    'ACC_y': filtered_chest['ACC'][:, 1],
+    'ACC_z': filtered_chest['ACC'][:, 2],
+    'ECG': filtered_chest['ECG'].flatten(),
+    'EMG': filtered_chest['EMG'].flatten(),
+    'EDA': filtered_chest['EDA'].flatten(),
+    'Temp': filtered_chest['Temp'].flatten(),
+    'Resp': filtered_chest['Resp'].flatten(),
+    'label': filtered_labels.flatten()
+})
 
 print("Unique labels:", np.unique(labels))
 
