22fae279b03b2927dc120bb8824c79f5f0be15a6
Mariamtarekk1 <mariamt6876@gmail.com>
2026-04-28 17:29:12 +0300

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -9,3 +9,21 @@ unique, counts = np.unique(labels, return_counts=True)
 for u, c in zip(unique, counts):
     print(f"Label {u}: {c}")
   
+df_sample = (
+    df.groupby('label', group_keys=False)
+      .apply(lambda x: x.sample(n=min(len(x), 25000), random_state=42))
+      .reset_index(drop=True)
+)
+
+df_sample = df_sample.sort_index()
+df = pd.DataFrame({
+    'ACC_x': filtered_chest['ACC'][:, 0],
+    'ACC_y': filtered_chest['ACC'][:, 1],
+    'ACC_z': filtered_chest['ACC'][:, 2],
+    'ECG': filtered_chest['ECG'].flatten(),
+    'EMG': filtered_chest['EMG'].flatten(),
+    'EDA': filtered_chest['EDA'].flatten(),
+    'Temp': filtered_chest['Temp'].flatten(),
+    'Resp': filtered_chest['Resp'].flatten(),
+    'label': filtered_labels.flatten()
+})
